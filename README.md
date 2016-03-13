ca41c2a915b75cf6e794c48197da351923a18710
SETKEH NRNS <SETKEH NRNS>
2016-03-14 02:07:45 +1100

    Added Code Comments and README

---
+++ README.md
@@ -0,0 +1,10 @@
+XKCD BOT
+========
+
+This application was written to answer a question on reddit (The one linked in the project description).
+
+It is not designed to be a full solution but should be sufficiant to run in a scheduler to download the daily comic.
+
+There are far more elegant ways of implimenting this kind of utility if you get banned or it does somthing crazy and explodes your brain remember it was not my fault and i take no responcibility :P
+
+Hope this is Helpfull.
--- xkcdbot/Program.cs
+++ xkcdbot/Program.cs
@@ -7,20 +7,32 @@ namespace xkcdBot
 {
     class Program
     {
+        // Set your Commic download Directory.
         string ComicDir = @"C:\Users\setkeh\Desktop\Comics";
+
+        // Specify the metadata URL for the commic you wish to download *Set to the Latest Comic by Default*
+        static string metadata = "http://xkcd.com/info.0.json";
+
         static void Main(string[] args)
         {
+            // Start The Application.
             MakeRequest(CreateRequest());
         }
 
         public static string CreateRequest()
         {
-            string UrlRequest = "http://xkcd.com/info.0.json";
+            // This provides the Direct URL to the metadata.
+            string UrlRequest = metadata;
             return (UrlRequest);
         }
 
         public static void MakeRequest(string requestUrl)
         {
+            /* 
+            This is where most of the magic Happens.
+            Here is where Network issues could throw exceptions hence why they are handled with try/catch.
+            This section makes the HTTP Request to xkcd for the JSON Metadata and Parses it into a JSON string.
+            */
             try
             {
                 HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest;
@@ -32,9 +44,11 @@ namespace xkcdBot
                         response.StatusCode,
                         response.StatusDescription));
 
+                    // This is the Parser that takes the metadata and produces a readable string.
                     string rawJson = new StreamReader(response.GetResponseStream()).ReadToEnd();
                     JObject Pjson = JObject.Parse(rawJson);
 
+                    // This simply begins the next non-static function.
                     Program p = new Program();
                     p.DownloadComic(Pjson);
                 }
@@ -46,6 +60,7 @@ namespace xkcdBot
             }
         }
 
+        // This does what it says on the tin it grabs the image url of the comic and downloads it the the Directory you specified in string ComicDir;
         public void DownloadComic(JObject Json)
         {
             string remoteFileUrl = Json.SelectToken("img").ToString();
